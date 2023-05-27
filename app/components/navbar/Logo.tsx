1eebe30c18f9cb570b2bb1aece2c6e87924697a6
ali26kh26 <fcb.akh.26@gmail.com>
2023-05-27 14:36:11 +0330

    add navbar UI

---
+++ app/components/navbar/Logo.tsx
@@ -0,0 +1,18 @@
+"use client";
+import { useRouter } from "next/navigation";
+import Image from "next/image";
+
+const Logo = () => {
+  const router = useRouter();
+  return (
+    <Image
+      src={"/images/logo.png"}
+      alt="logo"
+      width={100}
+      height={100}
+      className="hidden md:block"
+    />
+  );
+};
+
+export default Logo;
