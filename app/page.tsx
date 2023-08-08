b1c72a0dca17d030d6718e31093a89448342badc
ali26kh26 <fcb.akh.26@gmail.com>
2023-08-08 09:22:47 +0330

    change getListing action to get user properties

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,16 @@
 import getCurrentUser from "./actions/getCurrentUser";
-import getListings from "./actions/getListings";
+import getListings, { IListingParams } from "./actions/getListings";
 import ListingCard from "./components/listings/ListingCard";
 import ClientOnly from "./components/shared/ClientOnly";
 import Container from "./components/shared/Container";
 import EmptyState from "./components/shared/EmptyState";
 
-export default async function Home() {
-  const listings = await getListings();
+interface HomeProps {
+  searchParams: IListingParams;
+}
+
+export default async function Home({ searchParams }: HomeProps) {
+  const listings = await getListings(searchParams);
   const currentUser = await getCurrentUser();
   if (listings.length === 0) {
     return (
